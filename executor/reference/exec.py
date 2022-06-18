168a54695ede242dd97bba8d96295019257c3874
Max <max@sub1337.pro>
2022-06-18 15:10:35 +0300

    exec py

--- executor/reference/exec.py
+++ executor/reference/exec.py
@@ -1,3 +1,6 @@
+from ast import Str
+
+
 inp = """
 use console;
 main()
@@ -6,14 +9,27 @@ main()
 }
 """
 
+class CodeBit():
+    def __init__(self, str):
+        self.bit = str
+
 class Code:
-    def __init__(self, code : str):
-        self.parsed = code.split("\n")
+    code_bits = None
+    def __init__(self, code):
+        if type(code) is Code:
+            self.bits = code.bits
+        elif type(code) is str:
+            self.parse(code)
+    def parse(self, code):
+        parsed = code.split("\n")
+        self.code_bits = [CodeBit(bit) for bit in parsed]
         
 class Parser:
     code = None
+    def __init__(self, code):
+        self.parse(code)
     def parse(self,code):
-        if code is Code:
-            self.code = Code(code)
-        elif code is str:
-            self.code = Code()
+        self.code = Code(code)
+
+parser = Parser(inp)
+pass
