226258cc8bda777372cf47b8909bbc5c9cd00c14
Max <max@sub1337.pro>
2023-02-28 11:36:46 +0300

    Some more code

--- executor/source/executor.cpp
+++ executor/source/executor.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <deque>
 #include <array>
+#include <map>
 #include <memory>
 
 namespace MetaCLR
@@ -36,14 +37,20 @@ namespace MetaCLR
 	{
 
 	public:
-		int d;
+		int a;
+		int b;
+		int c;
 		Data()
 		{
-			d = 0;
+			a = 0;
+			b = 0;
+			c = 0;
 		}
-		Data(int d) 
+		Data(int a, int b, int c) 
 		{
-			d = d;
+			this->a = a;
+			this->b = b;
+			this->c = c;
 		}
 		~Data()
 		{
@@ -66,27 +73,43 @@ namespace MetaCLR
 		{
 			Command p_a;
 			p_a.commandType = enumCommand::alloc;
+			p_a.data.a = 1; // 1st var
+
 			Command p_a2;
 			p_a2.commandType = enumCommand::let;
-			p_a2.data = 2;
+			p_a2.data.a = 1; // 1st var
+			p_a2.data.b = 2;
 
 			Command p_b;
 			p_b.commandType = enumCommand::alloc;
+			p_b.data.a = 2; // 2nd var
 			Command p_b2;
 			p_b2.commandType = enumCommand::let;
-			p_b2.data = 3;
+			p_b2.data.a = 2; // 2nd var
+			p_b2.data.b = 3;
 
 			Command p_c;
-			p_c.commandType = enumCommand::math_plus;
+			p_c.commandType = enumCommand::alloc;
+			p_c.data.a = 3; // 3rd var
+
+			Command p_c2;
+			p_c2.commandType = enumCommand::math_plus;
+			p_c2.data.a = 3; // 3st var (result)
+			p_c2.data.b = 1; // 1st var
+			p_c2.data.c = 2; // 2nd var
+
 
 			Command p_print;
 			p_print.commandType = enumCommand::call;
-			p_print.data = 1;
+			p_print.data.a = 1; // function #1
+			p_print.data.b = 3; // third var
+
 			Commands.push_front(p_a);
 			Commands.push_front(p_a2);
 			Commands.push_front(p_b);
 			Commands.push_front(p_b2);
 			Commands.push_front(p_c);
+			Commands.push_front(p_c2);
 			Commands.push_front(p_print);
 		}
 		Command NextCommand()
@@ -108,11 +131,45 @@ namespace MetaCLR
 	class Executor
 	{
 		Code currCode;
+		std::map<int, int> memory;
+		//std::map<int, void*> memory_func;
 		void exec(Command currCommand)
 		{
-			if (currCommand == enumCommand::alloc)
+			if (currCommand.commandType == enumCommand::alloc)
 			{
-
+				memory.insert({ currCommand.data.a,0 });
+			}
+			if (currCommand.commandType == enumCommand::let)
+			{
+				memory[currCommand.data.a] = currCommand.data.b;
+			}
+			if (currCommand.commandType == enumCommand::math_plus)
+			{
+				memory[currCommand.data.a] = memory[currCommand.data.b] + memory[currCommand.data.c];
+			}
+			if (currCommand.commandType == enumCommand::math_minus)
+			{
+				memory[currCommand.data.a] = memory[currCommand.data.b] - memory[currCommand.data.c];
+			}
+			if (currCommand.commandType == enumCommand::math_mod)
+			{
+				memory[currCommand.data.a] = memory[currCommand.data.b] % memory[currCommand.data.c];
+			}
+			if (currCommand.commandType == enumCommand::math_mult)
+			{
+				memory[currCommand.data.a] = memory[currCommand.data.b] * memory[currCommand.data.c];
+			}
+			if (currCommand.commandType == enumCommand::math_divide)
+			{
+				memory[currCommand.data.a] = memory[currCommand.data.b] / memory[currCommand.data.c];
+			}
+			if (currCommand.commandType == enumCommand::call)
+			{
+				switch (currCommand.data.a)
+				{
+				case 1:
+					std::cout << currCommand.data.b;
+				}
 			}
 		}
 	public:
