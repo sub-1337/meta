5257ce2bcc603933650f13310f85e3ec39f7a378
Maxim <maxim.k.elasnce@gmail.com>
2024-07-22 23:27:26 +0300

    Описал типы

--- MetaExec/prototype/mpy/table.py
+++ MetaExec/prototype/mpy/table.py
@@ -1,7 +1,32 @@
-import enum
+from enum import Enum
 
-class keywords(enum):
+
+
+
+class keywords(Enum):
     var = "var"
-    fn = "fn"
+    function = "fn"
     new = "new"
+    auto = "auto"
     comment = "//"
+    const = "const"
+    semicolon = ";"
+    dollar = "$"
+    # example table.keywords.math_keywords.value.mod
+    class math_keywords(Enum):
+        plus = "+"
+        minus = "-"
+        mult = "*"
+        div = "/"
+        mod = "%"
+    class embeded_types(Enum):
+        char = "char"
+        string = "string"
+        int = "int"
+        real = "real"
+        pointer = "ptr"
+
+
+    
+       
+
